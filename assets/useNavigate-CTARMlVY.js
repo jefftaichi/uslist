70857e03b12de3f7d38cc0f753d61833f5673fd8
USList publisher <publisher@uslist.info>
2026-09-09 22:16:13 +0000

    Publish changed pages 2026-09-09T22:15:40Z

---
+++ assets/useNavigate-CTARMlVY.js
@@ -0,0 +1,1 @@
+import{r as e}from"./jsx-runtime-BkSabwWG.js";import{H as t,r as n}from"./link-D9TkvXEW.js";var r=e(t(),1);function i(e){let t=n();return r.useCallback(n=>t.navigate({...n,from:n.from??e?.from}),[e?.from,t])}export{i as t};
